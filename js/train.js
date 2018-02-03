4698fa519fbab082b6075663e9ec4cfac4c2a330
bthipavo <bthipavo@gmail.com>
2018-02-03 13:36:05 -0800

    added next time and minutes

--- js/train.js
+++ js/train.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    update();
+    setInterval(update, 60000);
+});
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyC1MN7IcakRMEECZGf_Zp2RZb3QYfsmY28",
@@ -25,7 +29,8 @@
   	trainName = $('#trainName').val().trim()
   	destination = $('#destination').val().trim()
   	firstTrainTime = $('#firstTrainTime').val()
-  	frequency = $('#frequency').val().trim()
+	frequency = $('#frequency').val().trim()
+	console.log("first train time " + firstTrainTime)
 
   	var tempData = {
   		name: trainName,
@@ -37,22 +42,39 @@
   	database.ref().push(tempData)
   })
 
-  database.ref().on('child_added', function(childSnapshot, prevChildKey) {
-  	console.log(childSnapshot.val())
+function update() {
+	$("#train-table > tbody").html("")
+	database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+		console.log(childSnapshot.val())
 
 
-  	var trainName = childSnapshot.val().name
-  	var destination = childSnapshot.val().destination
-  	var initialTrainTime = childSnapshot.val().initialTrainTime
-  	var frequency = childSnapshot.val().frequency
+		var trainName = childSnapshot.val().name
+		var destination = childSnapshot.val().destination
+		var TrainTime = childSnapshot.val().initialTrainTime
+		var frequency = childSnapshot.val().frequency
 
+		var currentTime = moment()
+		console.log(currentTime)
 
-  	console.log(trainName)
-  	console.log(destination)
-  	console.log(initialTrainTime)
-  	console.log(frequency)
+		var initialTrainTime = moment(TrainTime, "hh:mm").subtract('1, years')
+		var trainDifference = currentTime.diff(initialTrainTime, "minutes")
+		var nextTrain = Math.abs(trainDifference % frequency)
+		var nextTrainMinutes = parseInt(frequency) - nextTrain
+		var nextTrainTime = currentTime.add(nextTrainMinutes, "minutes").format("hh:mm a")
 
-	$("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
-  initialTrainTime + "</td><td>" + frequency + "</td></tr>");
-
-  })
+		console.log(trainName)
+		console.log(destination)
+		console.log("train time snap " + TrainTime)
+		console.log("next train time " + initialTrainTime.format('LT'))
+		console.log("difference " + trainDifference)
+		console.log(trainDifference/frequency)
+		console.log(nextTrain)
+		console.log("next Train minutes " +  nextTrainMinutes)
+		// console.log("next train time " + currentTime - nextTrain)
+		console.log(frequency)
+		
+		$("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+		initialTrainTime.format("hh:mm a") + "</td><td>" + frequency + " minutes" + "</td><td>" + nextTrainTime + "</td><td>" +
+		nextTrainMinutes + "</td></tr>");
+	})
+}
